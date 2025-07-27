dddd8bb47fe88064ea2202dbeae6e643a7298c79
CodeExplorerX <CodeExplorerX@proton.me>
2025-07-27 12:26:01 +0200

    refactor TestHello: add subtests

--- main.go
+++ main.go
@@ -5,9 +5,12 @@ import "fmt"
 const englishHelloPreix = "Hello, "
 
 func Hello(name string) string {
+	if name == "" {
+		name = "World"
+	}
 	return englishHelloPreix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("World"))
 }
