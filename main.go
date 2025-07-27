2e343fd6acf81b20bd2873b475f10becae53625d
CodeExplorerX <CodeExplorerX@proton.me>
2025-07-27 12:46:29 +0200

    feature Hello(): Add language

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import "fmt"
 
 const englishHelloPreix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHelloPreix + name
 }
 
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", "English"))
 }
