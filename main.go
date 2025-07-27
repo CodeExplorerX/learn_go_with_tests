976f1b28d782b65fbd3f49840900f3b97ade6103
CodeExplorerX <CodeExplorerX@proton.me>
2025-07-27 12:56:16 +0200

    refactor Hello(): Add new language and create greeting function

--- main.go
+++ main.go
@@ -2,17 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPreix = "Hello, "
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPreix  = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if language == "Spanish" {
-		return "Hola, " + name
+	return greetingPreifx(language) + name
+}
+
+func greetingPreifx(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPreix
 	}
-	return englishHelloPreix + name
+
+	return
 }
 
 func main() {
--- main_test.go
+++ main_test.go
@@ -22,6 +22,14 @@ func TestHello(t *testing.T) {
 
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+
+		assertCorrectMessage(t, got, want)
+	})
+
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
